16584a1c77ade1af75344a470bf9f9a8ca2e2be2
disko998 <disko998@gmail.com>
2020-03-02 13:23:37 +0100

    Updates

---
+++ precache-manifest.7f563d9e9230059dc89bbf587524baf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2a9ff80da36f33694fba05b4f57211",
+    "url": "/disko998/my-todos/index.html"
+  },
+  {
+    "revision": "ecb1d0e61a8e2c192c73",
+    "url": "/disko998/my-todos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f19d512d55211f1833b",
+    "url": "/disko998/my-todos/static/js/2.f55ab54f.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/disko998/my-todos/static/js/2.f55ab54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb1d0e61a8e2c192c73",
+    "url": "/disko998/my-todos/static/js/main.9da0e1b3.chunk.js"
+  },
+  {
+    "revision": "8f12828eae615a7385b7",
+    "url": "/disko998/my-todos/static/js/runtime-main.25722250.js"
+  }
+]);
